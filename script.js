594865b5292ea86590bdb0639e0cbdf5098a0d03
rifatDemirok <121319235+rifatDemirok@users.noreply.github.com>
2024-02-20 01:00:24 +0300

    Previous work has been published on the Web. And Script.css file has ben added.

--- script.js
+++ script.js
@@ -6,94 +6,112 @@ const antalya="Antalya";
 const muğla="Muğla";
 
 let cities = new Array();
+
+
 for (let i = 0; i < 4; i++) {
     cities.push(konya);
 }
-
-console.log("********************Pushed Izmir*******************");
-console.log(cities);
+let outputDivPush = document.getElementById("pushedIzmir");
+outputDivPush.innerHTML += "<h1>Pushed Izmir</h1><br>";
+outputDivPush.innerHTML +="<h4>"+"Current Array====>"+ cities.toString() +"</h4>"+ "<br>";
 cities.push(izmir);
-console.log(cities);
+outputDivPush.innerHTML+="<h4>"+"Cities.push(izmir)====>"+cities.toString() +"</h4>"+ "<br>";
 
-cities.pop();
-
-console.log("********************Unshift Izmir*******************");
-console.log(cities);
+let outputDivUnshift = document.getElementById("unShiftIzmir");
+outputDivUnshift.innerHTML += "<h1>UnShift Izmir</h1><br>";
+outputDivUnshift.innerHTML +="<h4>"+"Current Array====>"+ cities.toString() +"</h4>"+ "<br>";
 cities.unshift(izmir);
-console.log(cities);
+outputDivUnshift.innerHTML+="<h4>"+"cities.unshift(izmir);====>"+cities.toString() +"</h4>"+ "<br>";
 
 cities=[];
-
-console.log("********************Pop İzmir*******************");
 cities.push(konya, istanbul,ankara,izmir);
-console.log(cities);
+let outputDivPop = document.getElementById("popIzmir");
+outputDivPop.innerHTML += "<h1>Pop Izmir</h1><br>";
+outputDivPop.innerHTML +="<h4>"+"Current Array====>"+ cities.toString() +"</h4>"+ "<br>";
 cities.pop(izmir);
-console.log(cities);
+outputDivPop.innerHTML+="<h4>"+"cities.pop(izmir);====>"+cities.toString() +"</h4>"+ "<br>";
 
 cities=[];
 
-console.log("********************Shift*******************");
 cities.push(konya, istanbul,ankara,izmir);
-console.log(cities);
+let outputDivShift = document.getElementById("Shift");
+outputDivShift.innerHTML += "<h1>Shift</h1><br>";
+outputDivShift.innerHTML +="<h4>"+"Current Array====>"+ cities.toString() +"</h4>"+ "<br>";
 cities.shift();
-console.log(cities);
+outputDivShift.innerHTML+="<h4>"+"cities.shift();====>"+cities.toString() +"</h4>"+ "<br>";
 
 cities=[];
-
-console.log("********************Map(Muğla)*******************");
 for (let i = 0; i < 4; i++) {
     cities.push(izmir);
 }
-console.log(cities);
-const NewCities=cities.map(Element=>muğla);
-console.log(NewCities);
+let outputDivMap = document.getElementById("map");
+outputDivMap.innerHTML += "<h1>Map</h1><br>";
+outputDivMap.innerHTML +="<h4>"+"Current Array====>"+ cities.toString() +"</h4>"+ "<br>";
+newArrMugla=cities.map(Element=>muğla);
+outputDivMap.innerHTML+="<h4>"+"cities.map(Element=>muğla);====>"+newArrMugla.toString() +"</h4>"+ "<br>";
 
 cities=[];
-
-console.log("********************Filter(IZmir)*******************");
 cities.push(konya,izmir,ankara,izmir);
-console.log(cities);
-const filteredCities=cities.filter(Element=>Element==izmir);
-console.log(filteredCities);
 
+let outputDivFilter = document.getElementById("filter");
+outputDivFilter.innerHTML += "<h1>Filter</h1><br>";
+outputDivFilter.innerHTML +="<h4>"+"Current Array====>"+ cities.toString() +"</h4>"+ "<br>";
+const newarr= cities.filter(Element=>Element==izmir);
+outputDivFilter.innerHTML+="<h4>"+"cities.filter(Element=>Element==izmir);====>"+newarr.toString() +"</h4>"+ "<br>";
 
-
-console.log("********************Reverse*******************");
-console.log(cities);
+let outputDivReverse = document.getElementById("Reverse");
+outputDivReverse.innerHTML += "<h1>Reverse</h1><br>";
+outputDivReverse.innerHTML +="<h4>"+"Current Array====>"+ cities.toString() +"</h4>"+ "<br>";
 cities.reverse();
-console.log(cities);
+outputDivReverse.innerHTML+="<h4>"+"cities.reverse();====>"+cities.toString() +"</h4>"+ "<br>";
 
 cities.reverse();
 
-console.log("********************Includes(Izmir)*******************");
-console.log(cities);
-console.log( cities.includes(izmir));
+let outputDivInclude = document.getElementById("Include");
+outputDivInclude.innerHTML += "<h1>Include</h1><br>";
+outputDivInclude.innerHTML +="<h4>"+"Current Array====>"+ cities.toString() +"</h4>"+ "<br>";
+let tf=cities.includes(izmir)
+outputDivInclude.innerHTML+="<h4>"+"cities.includes(izmir);====>"+tf +"</h4>"+ "<br>";
 
-console.log("********************At(index of 3)*******************");
-console.log(cities);
-console.log(cities.at(2));
+let outputDivIndexOf = document.getElementById("indexof");
+outputDivIndexOf.innerHTML += "<h1>Index Of 3</h1><br>";
+outputDivIndexOf.innerHTML +="<h4>"+"Current Array====>"+ cities.toString() +"</h4>"+ "<br>";
+let Io=cities.at(2)
+outputDivIndexOf.innerHTML+="<h4>"+"cities.at(2)====>"+Io +"</h4>"+ "<br>";
 
-console.log("********************Slice(0,1)*******************");
-console.log(cities);
-console.log(cities.slice(0,1));
+let outputDivSlice = document.getElementById("slice");
+outputDivSlice.innerHTML += "<h1>Slice</h1><br>";
+outputDivSlice.innerHTML +="<h4>"+"Current Array====>"+ cities.toString() +"</h4>"+ "<br>";
+cities.splice(0,1)
+outputDivSlice.innerHTML+="<h4>"+"cities.splice(0,1)====>"+cities.toString() +"</h4>"+ "<br>";
+
 
 cities=[];
-console.log("********************Splice*******************");
-cities.push(konya,izmir,ankara,izmir);// bazı kaynakları taradım slice ve splice fonksiyonları aynı işi yapıyorlar. anlamadım.
-console.log(cities);                  //splice (1,1,xxx,xxx) metodu da aynısını yapmadı. farklı bir şekilde çalıştı. anlamadım.  
-let splice1=cities.splice(0,1);
-console.log(splice1);
+cities.push(konya,izmir,ankara,izmir);
+let outputDivSplice = document.getElementById("splice");
+outputDivSplice.innerHTML += "<h1>Splice</h1><br>";
+outputDivSplice.innerHTML +="<h4>"+"Current Array====>"+ cities.toString() +"</h4>"+ "<br>";
+cities.slice(0,1)
+outputDivSplice.innerHTML+="<h4>"+"cities.slice(0,1)====>"+Io +"</h4>"+ "<br>";
 
-console.log("********************Fill(Antalya)*******************");
-console.log(cities);
-console.log(cities.fill(antalya));
+let outputDivFill = document.getElementById("fill");
+outputDivFill.innerHTML += "<h1>Fill(Antalya)</h1><br>";
+outputDivFill.innerHTML +="<h4>"+"Current Array====>"+ cities.toString() +"</h4>"+ "<br>";
+cities.fill(antalya)
+outputDivFill.innerHTML+="<h4>"+"cities.fill(antalya)====>"+cities.toString() +"</h4>"+ "<br>";
 
 cities=[];
-console.log("********************Find(Izmir)*******************");
 cities.push(konya,izmir,ankara,izmir)
-console.log(cities);
-console.log(cities.find(finder=>finder=="Izmir"));
 
-console.log("********************FindIndex(Izmir)*******************");
-console.log(cities);
-console.log(cities.indexOf("Izmir"));
+let outputDivFind = document.getElementById("Find");
+outputDivFind.innerHTML += "<h1>Find(Izmir)</h1><br>";
+outputDivFind.innerHTML +="<h4>"+"Current Array====>"+ cities.toString() +"</h4>"+ "<br>";
+const finder=cities.find(finder=>finder=="Izmir")
+outputDivFind.innerHTML+="<h4>"+"cities.find(finder=>finder==Izmir)====>"+finder +"</h4>"+ "<br>";
+
+let outputDivFindIndex = document.getElementById("FindIndex");
+outputDivFindIndex.innerHTML += "<h1>FindIndex(Izmir)</h1><br>";
+outputDivFindIndex.innerHTML +="<h4>"+"Current Array====>"+ cities.toString() +"</h4>"+ "<br>";
+const index=(cities.indexOf("Izmir"));
+outputDivFindIndex.innerHTML+="<h4>"+"(cities.indexOf(Izmir));====>"+index +"</h4>"+ "<br>";
+
