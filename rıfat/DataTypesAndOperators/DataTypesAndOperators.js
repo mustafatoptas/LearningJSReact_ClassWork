b94f06d2c209cf0d06bb40470d94ab7bbfcfbe4d
rifatDemirok <121319235+rifatDemirok@users.noreply.github.com>
2024-03-02 11:48:08 +0300

    Data Types and operators has added. script.js has modified but thats need some fixing

---
+++ rıfat/DataTypesAndOperators/DataTypesAndOperators.js
@@ -0,0 +1,63 @@
+
+var typeDiv = document.createElement("div");
+typeDiv.classList.add("array-div");
+typeDiv.setAttribute("id", "string");
+document.body.appendChild(typeDiv);
+
+let DivPush = document.getElementById("string");
+DivPush.innerHTML += "<h1> Data Type</h1><br>";
+DivPush.innerHTML +=
+  "<h4>" + "'Rıfat' is ====>" + typeof "Rıfat" + "<br>" +
+           "3.14 is    ====>" + typeof 3.14 + "<br>" +
+           "NaN is     ====>" + typeof NaN + "<br>" +
+           "false is   ====>" + typeof false + "<br>" +
+           "[1, 2, 3, 4] is           ====>" + typeof [1, 2, 3, 4] + "<br>" +
+           "{name:'Rıfat', age:22} is ====>" + typeof {name:'Rıfat', age:22} + "<br>" +
+           "new Date() is             ====>" + typeof new Date() + "<br>" +
+           "function () {} is         ====>" + typeof function () {} + "<br>" +
+           "myCar is                  ====>" + typeof myCar + "<br>" +
+           "null is                   ====> " + typeof null; + "</h4>" + "<br>";
+
+var operatorDiv = document.createElement("div");
+operatorDiv.classList.add("array-div");
+operatorDiv.setAttribute("id", "operator");
+document.body.appendChild(operatorDiv);
+let x=5;
+let y=2;
+let DivPush2 = document.getElementById("operator");
+DivPush2.innerHTML += "<h1>Maths Operators</h1><br>";
+DivPush2.innerHTML +=
+  "<h4>" + "x = 5; y = 2; "+"<br> "+
+  "x + y is ====>" + (x+y) + "<br>" +
+           "x - y is ====>" + (x-y) + "<br>" +
+           "x * y is ====>" + (x*y) + "<br>" +
+           "x / y is ====>" + (x/y) + "<br>" +
+           "x % y is ====>" + (x%y) + "<br>" +
+           "x ** y is ====>" + (x**y) + "<br>" +
+
+           
+           "x += y is ====>" + (x+=y) + "<br>" +
+           "x -= y is ====>" + (x-=y) + "<br>" +
+           "x *= y is ====>" + (x*=y) + "<br>" +
+           "x /= y is ====>" + (x/=y) + "<br>" +
+           "x %= y is ====>" + (x%=y) + "<br>" +
+           "x **= y is ====>" + (x**=y) + "<br>" ;
+
+var mantiksalDiv = document.createElement("div");
+mantiksalDiv.classList.add("array-div");
+mantiksalDiv.setAttribute("id", "mantiksal");
+document.body.appendChild(mantiksalDiv);
+let DivPush3 = document.getElementById("mantiksal");
+DivPush3.innerHTML += "<h1>Logical Operators</h1><br>";
+DivPush3.innerHTML +=
+           "<h4>"+"x == y is ====> Eşit midir? (false)<br>" +
+           "x === y is ====> Aynı ve Eşit midir? (false)<br>" +
+           "x != y is ====> Eşit değil midir? (true)<br>" +
+           "x !== y is ====> Aynı ve Eşit değil midir? (true)<br>" +
+           "x > y is ====> x, y'den büyüktür. (true)<br>" +
+          
+           "x >= y is ====> x, y'den büyük veya eşittir. (true)<br>" +
+           
+           "x && y > 1 is ====> x ve y 1'den büyük müdür? (true)<br>";
+
+
--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 function loadContent(topic) {
-  var filePath = topic + "/" + topic + ".js";
-  console.log(topic);
+  let filePath = topic + "/" + topic + ".js";
   fetch(filePath)
     .then((response) => response.text())
     .then((data) => {
-      var script = document.createElement("script");
-      script.textContent = data;
-      document.body.appendChild(script);
-      console.log(script);
+      var existingScript = document.getElementById("dynamic-script");
+      if (existingScript !== null) {
+        console.log(
+          "exist script before deleting " + existingScript.textContent//silmeden önce oluşan scripti existing olarak alabiliyor.
+        );
+        existingScript.textContent = "";
+        console.log("exist script after deleting" + existingScript.textContent);//silme işlemi gerçekleşiyor. sorun yok
+
+        existingScript.textContent = data;
+        console.log("exist script pushed " + existingScript.textContent);//yeni script basılıyor burada da bir sorun yok.
+        window.location.assign(
+          "http://127.0.0.1:5500/r%C4%B1fat/r%C4%B1fat.html"//bu kod gereksiz. yukarıdaki her şey çalışıyor fakat yeni script sayfaya basılmıyor. 
+                                                            // ben de sayfayı yeniden yükleyip yeni scripti çalıştırmak istedim.
+                                                            // yani yeni script datası alınıyor görünüyor fakat eski script çalışmaya devam ediyor.
+                                                            //ve yeni menuyu load etmemiz için iki kez tıklamamız gerekiyor. buradaki sorun da bu.
+        );
+        //document.body.appendChild(existingScript);
+      } else {
+        var script = document.createElement("script");
+        script.id = "dynamic-script";
+        script.textContent = data;
+        document.body.appendChild(script);
+        console.log("orj script:" + script.textContent);
+      }
     })
+
     .catch((error) => console.error("Error:", error));
 }
